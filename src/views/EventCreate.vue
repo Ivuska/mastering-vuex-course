001f05ec58b892cac4b9006c89b16f97493aa8cd
Ivuska <fischerova.ivka@gmail.com>
2022-08-13 22:31:08 +0200

    Accesing state, Getters for derived state, Mapping to Computed.

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEventById(2) }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+  export default {
+    computed: { 
+      // Spread operator, it is a part of JS not only Vue.
+      ...mapGetters(['getEventById']),
+      ...mapState (['user', 'categories']) }
+    }
+</script>
+
+<style lang="" scoped>
+
+</style>
