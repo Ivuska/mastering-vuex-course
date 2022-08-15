3baac4d3dcb2aee7837b6e96554d95e6447f3ab8
Ivuska <fischerova.ivka@gmail.com>
2022-08-15 09:40:22 +0200

    Mutations and actions part 2 - pagination, update the events

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
+// Great article https://medium.com/dailyjs/mastering-vuex-zero-to-hero-e0ca1f421d45
 
 export default new Vuex.Store({
   state: {
@@ -14,19 +15,26 @@ export default new Vuex.Store({
       { id: 3, text: '...', done: true },
       { id: 4, text: '...', done: false }
     ],
-    events: [
-      { id: 1, title: '...', organizer: '...' },
-      { id: 2, title: '...', organizer: '...' },
-      { id: 3, title: '...', organizer: '...' },
-      { id: 4, title: '...', organizer: '...' }
-    ]
+    events: [],
+    eventsTotal: 0,
+    event: {}
   },
-  // Mutations are synchronous.
+  // Mutations are synchronous, so we cannot run asynchronous operations such as API calls inside them.
   // This is the "pick up of the bread."
-  //Always put them within Actions.
+  // Always put them within Actions.
+  // Set the data into state.
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENTS_TOTAL(state, eventsTotal) {
+      state.eventsTotal = eventsTotal
+    },
+    SET_EVENT(state, event) {
+      state.event = event
     }
   },
   // Actions are asynchronous.
@@ -38,6 +46,40 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    // The payload (perPage, page in this example) in both Actions and Mutations can be a single variable OR an object.
+    // In this example it is object with two properties so we need to use curly brackets.
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(response => {
+          commit(
+            'SET_EVENTS_TOTAL',
+            parseInt(response.headers['x-total-count'])
+          )
+          commit('SET_EVENTS', response.data)
+        })
+        .catch(error => {
+          console.log('There was an error:', error.response)
+        })
+    },
+    // Acces our getters.
+    fetchEvent({ commit, getters }, id) {
+      // Try to find this event.
+      var event = getters.getEventById(id)
+
+      // If we found, set it.
+      if (event) {
+        commit('SET_EVENT', event)
+        // Else go fetch it.
+      } else {
+          EventService.getEvent(id)
+        .then(response => {
+          commit('SET_EVENT', response.data)
+        })
+        .catch(error => {
+          console.log('There was an error:', error.response)
+        })
+      }
     }
   },
   getters: {
